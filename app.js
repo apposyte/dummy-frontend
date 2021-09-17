b25a1f4062bf2adec0f7e604aff94f30b374a655
Jörg <joerg.landthaler@fylance.de>
2021-09-17 20:14:50 +0200

    +http auth

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
+// HTTP module
 const http = require("http");
+
+// Authentication module.
+const auth = require("http-auth");
+const basic = auth.basic({
+    realm: "test-ls-user",
+    file: __dirname + "/../data/users.htpasswd" // gevorg:gpass, Sarah:testpass
+});
 
 const uri = "http://localhost:5000";
 
 http
-    .createServer(function (request, response) {
+    .createServer(basic.check(function (request, response) {
         const proxy = http.request(uri);
         proxy.on("response", function (proxyResponse) {
             proxyResponse.pipe(response);
         });
         request.pipe(proxy);
-    })
-    .listen(8080);
+    }))
+    .listen(5000);
